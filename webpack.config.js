f6ae25e918f61a52ad1a8f0d1660ac3e3e14f18a
Miaow233 <miaow233@qq.com>
2021-11-23 23:42:50 +0800

    Format

--- webpack.config.js
+++ webpack.config.js
@@ -9,68 +9,68 @@ const CopyPlugin = require("copy-webpack-plugin");
 const isProduction = process.env.NODE_ENV == "production";
 
 const stylesHandler = isProduction
-    ? MiniCssExtractPlugin.loader
-    : "style-loader";
+  ? MiniCssExtractPlugin.loader
+  : "style-loader";
 
 const config = {
-    target: "web",
-    entry: {
-        index: "./src/index.js",
-        setting: "./src/setting.js"
-    },
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        clean: true,
-    },
-    devServer: {
-        open: true,
-        host: "0.0.0.0",
-        port: 3000
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./public/index.html",
-        }),
+  target: "web",
+  entry: {
+    index: "./src/index.js",
+    setting: "./src/setting.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devServer: {
+    open: true,
+    host: "0.0.0.0",
+    port: 3000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
 
-        new CopyPlugin([
-            {
-                from: path.resolve(__dirname, "./node_modules/blockly/media"),
-                to: path.resolve(__dirname, "dist/media")
-            }
-        ])
-        // Add your plugins here
-        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+    new CopyPlugin([
+      {
+        from: path.resolve(__dirname, "./node_modules/blockly/media"),
+        to: path.resolve(__dirname, "dist/media"),
+      },
+    ]),
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: [stylesHandler, "css-loader"],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/i,
+        type: "asset",
+      },
+
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
     ],
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/i,
-                loader: "babel-loader",
-            },
-            {
-                test: /\.css$/i,
-                use: [stylesHandler, "css-loader"],
-            },
-            {
-                test: /\.(eot|svg|ttf|woff|woff2)$/i,
-                type: "asset",
-            },
-
-            // Add your rules for custom modules here
-            // Learn more about loaders from https://webpack.js.org/loaders/
-        ],
-    },
+  },
 };
 
 module.exports = () => {
-    if (isProduction) {
-        config.mode = "production";
+  if (isProduction) {
+    config.mode = "production";
 
-        config.plugins.push(new MiniCssExtractPlugin());
+    config.plugins.push(new MiniCssExtractPlugin());
 
-        config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
-    } else {
-        config.mode = "development";
-    }
-    return config;
+    config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
+  } else {
+    config.mode = "development";
+  }
+  return config;
 };
